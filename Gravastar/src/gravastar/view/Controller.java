49394d814bb8157345a3a00e223f8af4358d0676
Dominic <dominoinator@gmail.com>
2019-11-03 16:37:35 -0500

    Started Project

---
+++ Gravastar/src/gravastar/view/Controller.java
@@ -0,0 +1,40 @@
+package gravastar.view;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class Controller extends VBox
+{
+    public Button domsButton;
+    public TextField commandBar;
+    public TextArea primaryTextArea;
+    public VBox primaryVBox;
+
+    public boolean isFirstLine = true;
+
+    public void handleButtonClick()
+    {
+        domsButton.setText("Hello World");
+    }
+
+    public void handleCommandEntered()
+    {
+        addLine(commandBar.getText());
+        commandBar.setText("");
+    }
+
+    public void addLine(String newText)
+    {
+        if(isFirstLine)
+        {
+            primaryTextArea.setText(newText);
+            isFirstLine = false;
+        }
+        else
+        {
+            primaryTextArea.setText(primaryTextArea.getText() + '\n' + newText);
+        }
+    }
+}
