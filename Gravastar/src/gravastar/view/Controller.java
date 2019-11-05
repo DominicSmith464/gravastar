1ca5c0a0e70df8d5a1780494c7ad782c26965981
Dominic <dominoinator@gmail.com>
2019-11-04 22:13:50 -0500

    Added the ability to create rectangles

--- Gravastar/src/gravastar/view/Controller.java
+++ Gravastar/src/gravastar/view/Controller.java
@@ -6,18 +6,25 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Paint;
 
 public class Controller extends VBox
 {
     public Button domsButton;
     public TextField commandBar;
     public TextArea primaryTextArea;
+    public TilePane invTilePane;
+    public Rectangle awesomeRect1;
 
     private boolean isFirstLine = true;
+    private Paint bluePaint = Paint.valueOf("0x0000FF");
 
     public void handleButtonClick()
     {
         domsButton.setText("Hello World");
+        Rectangle myRectum = new Rectangle(50, 50, bluePaint);
+        invTilePane.getChildren().add(myRectum);
     }
 
     public void handleCommandEntered()
